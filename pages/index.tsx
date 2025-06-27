f2f512507a14f6fa0905be5dc6b7ed84ffe38904
krzychu-25 <116654851+krzychu-25@users.noreply.github.com>
2025-06-27 12:39:06 +0200

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-600 to-blue-500">
-      <h1 className="text-white text-5xl font-bold">Witamy na Metzlii 🌌</h1>
-    </div>
-  )
-}
