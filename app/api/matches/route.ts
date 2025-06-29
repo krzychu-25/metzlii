c1c0aef67680101ed8c9629e871f4035ceb7d9d6
krzychu-25 <116654851+krzychu-25@users.noreply.github.com>
2025-06-29 21:07:03 +0200

    Update route.ts

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -1,6 +1,6 @@
 // app/api/matches/route.ts
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 
