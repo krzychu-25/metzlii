1fb31356e9c27eb1ad0146bec572888c27005dd3
krzychu-25 <116654851+krzychu-25@users.noreply.github.com>
2025-06-29 18:46:37 +0000

    Fix duplicated authOptions

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ovGtSD02PEwOodG7ql6bcwnwFVuuLqbCcvufLXFi0g8=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
