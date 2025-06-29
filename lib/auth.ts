f2b5e7ba0e6a67233dab08c59d0017298a7b8c9a
krzychu-25 <116654851+krzychu-25@users.noreply.github.com>
2025-06-29 20:34:01 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,17 @@
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import prisma from './prisma';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "./prisma";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  // inne opcje NextAuth
+};
 
 export const authOptions = {
   providers: [
